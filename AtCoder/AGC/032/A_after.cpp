c533bd8a1c80f22984b6a3ba2a78e009ad560cc6
robertcal <robertcal900@gmail.com>
2019-03-24 00:56:44 +0900

    AGC032のA問題

---
+++ AtCoder/AGC/032/A_after.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define INF 1e9
+using namespace std;
+
+int main() {
+
+    int N; cin >> N;
+
+    vector<int> b(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> b[i];
+    }
+
+    int b_size;
+    b_size = b.size();
+    vector<int> ans;
+
+    while (ans.size() < b_size) {
+        int max_j = -1;
+        for (int j = 0; j < b.size(); ++j) {
+            if (b[j] == j + 1) {
+                max_j = j;
+            }
+        }
+
+        if (max_j == -1) {
+            cout << -1 << endl;
+            return 0;
+        }
+
+        ans.push_back(b[max_j]);
+        b.erase(b.begin() + max_j);
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    for (int k = 0; k < N; ++k) {
+        cout << ans[k] << endl;
+    }
+}
