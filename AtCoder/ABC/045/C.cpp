bb190b49c06489b0e0d4e3f872dd1da4d9ed11ef
robertcal <robertcal900@gmail.com>
2019-03-28 12:52:29 +0900

    ABC045のCを解いた （答えを参考にしながらなので、まだ自分の力では解けない気がする…）

--- AtCoder/ABC/045/C.cpp
+++ AtCoder/ABC/045/C.cpp
@@ -9,16 +9,24 @@ int main() {
     int N;
     N = S.length();
 
-    int ans = 0;
+    long long ans = 0;
 
     for (int bit = 0; bit < (1 << N-1); ++bit) { //2^(N-1)のbit全探索
-        int sum = 0;
+        long long sum = 0;
 
-        for (int i = 0; i < N; ++i) {
-            if (bit & (1 << i)) {
-
+        long long tmp = S[0] - '0'; //文字列を整数に
+        for (int i = 0; i < N-1; ++i) {
+            if (bit & (1 << i)) { //+が挿入されたと考える
+                sum += tmp;
+                tmp = 0;
             }
+
+            tmp = tmp * 10 + S[i+1] - '0';
         }
 
+        sum += tmp; //最後の数が足されていないので足す
+        ans += sum;
     }
+
+    cout << ans << endl;
 }
