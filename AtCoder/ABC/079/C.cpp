fef06b6e5c2daf868a28e6746902df851165de63
robertcal <robertcal900@gmail.com>
2019-04-03 22:53:30 +0900

    ABC079のC問題（bit全探索）を解いた

--- AtCoder/ABC/079/C.cpp
+++ AtCoder/ABC/079/C.cpp
@@ -6,30 +6,27 @@ using namespace std;
 int main() {
 
     string S; cin >> S;
-    vector<int> N(4);
-    N[0] = S[0] - '0';
-    N[1] = S[1] - '0';
-    N[2] = S[2] - '0';
-    N[3] = S[3] - '0';
 
     for (int bit = 0; bit < (1 << 3); ++bit) {
-        int sum = N[0];
-        vector<char> OP(3);
+        int sum = S[0] - '0';
+        string word;
+        word += S[0];
 
         for (int i = 0; i < 3; ++i) {
             if (bit & (1 << i)) {
-                sum += N[i+1];
-                OP.push_back('+');
+                sum += S[i+1] - '0';
+                word += '+';
+                word += S[i+1];
             } else {
-                sum -= N[i+1];
-                OP.push_back('-');
+                sum -= S[i+1] - '0';
+                word += '-';
+                word += S[i+1];
             }
         }
 
         if (sum == 7) {
-            cout << S[0] + OP[0] + S[1] + OP[1] + S[2] + OP[2] + S[3] + "=7" << endl;
+            cout << word + "=7" << endl;
             return 0;
         }
     }
 }
-
