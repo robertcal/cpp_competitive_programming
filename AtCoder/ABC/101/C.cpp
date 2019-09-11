de8a228e282e03c385632480d282bc6bc0221e80
robertcal <robertcal900@gmail.com>
2019-09-11 22:02:30 +0900

    update ABC101_C

--- AtCoder/ABC/101/C.cpp
+++ AtCoder/ABC/101/C.cpp
@@ -8,19 +8,17 @@ int main() {
 
     int n, k; cin >> n >> k;
 
-    int a_1 = 0;
+    int ans = 0;
+    int right = 0;
 
-    for (int i = 0; i < n; ++i) {
-        int a; cin >> a;
-        if (a == 1) {
-            a_1 = i;
-            break;
+    while (right < n) {
+        if (ans == 0) {
+            right += k; //最初の区間はk分
+        } else {
+            right += k - 1; //次の区間からはk-1分だけ広げられる
         }
+        ++ans;
     }
-
-    int ans = INF;
-
-    ans = (a_1 + 1) / (k - 1) + (n - a_1) / (k - 1);
 
     cout << ans << endl;
 }
