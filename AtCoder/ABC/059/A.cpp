5adb3ff1eec2c12b2c956d43e8e93e19a241a37e
robertcal <robertcal900@gmail.com>
2019-11-04 22:44:15 +0900

    add ABC059_A

---
+++ AtCoder/ABC/059/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+#define INF 1e9
+using namespace std;
+typedef long long ll;
+
+int main() {
+
+    string s1, s2, s3; cin >> s1 >> s2 >> s3;
+
+    cout << (char)toupper(s1[0]) << (char)toupper(s2[0]) << (char)toupper(s3[0]) << endl;
+}
