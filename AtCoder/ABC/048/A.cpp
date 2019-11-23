e6318eebdf672c1162945998bb542d16ce79c8f7
robertcal <robertcal900@gmail.com>
2019-11-23 23:13:57 +0900

    add ABC048_A

---
+++ AtCoder/ABC/048/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+#define INF 1e9
+using namespace std;
+typedef long long ll;
+
+int main() {
+
+    string s1, s2, s3; cin >> s1 >> s2 >> s3;
+
+    cout << 'A' << s2[0] << 'C' << endl;
+}
