c80efde932e3cbf252f81c77bfb50b26b87dd949
robertcal <robertcal900@gmail.com>
2019-11-23 23:06:31 +0900

    add ABC049_AB

---
+++ AtCoder/ABC/049/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define INF 1e9
+using namespace std;
+typedef long long ll;
+
+int main() {
+
+    char c; cin >> c;
+
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+        puts("vowel");
+    } else {
+        puts("consonant");
+    }
+}
---
+++ AtCoder/ABC/049/B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define INF 1e9
+using namespace std;
+typedef long long ll;
+
+int main() {
+
+    ll h, w; cin >> h >> w;
+
+    vector<string> c(h);
+    for (ll i = 0; i < h; ++i) {
+        cin >> c[i];
+
+        cout << c[i] << endl;
+        cout << c[i] << endl;
+    }
+}
