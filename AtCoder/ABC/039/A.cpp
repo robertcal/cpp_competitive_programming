c7a1c3fcb38fe71a669177da79df14708f17a8dc
robertcal <robertcal900@gmail.com>
2019-11-28 22:26:20 +0900

    add ABC039_AB

---
+++ AtCoder/ABC/039/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+#define INF 1e9
+using namespace std;
+typedef long long ll;
+
+int main() {
+
+    int a, b, c; cin >> a >> b >> c;
+
+    cout << (a * b) * 2 + (b * c) * 2 + (c * a) * 2 << endl;
+}
