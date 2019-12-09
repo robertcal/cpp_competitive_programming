5fd0502a2e4c4966b118096bc695cd360abe7181
robertcal <robertcal900@gmail.com>
2019-12-09 19:54:22 +0900

    add ABC020_AB

---
+++ AtCoder/ABC/020/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+int main() {
+
+    int q; cin >> q;
+
+    if (q == 1) {
+        puts("ABC");
+    } else {
+        puts("chokudai");
+    }
+}
