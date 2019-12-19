00ee8dd7253f6a0db7e4e9b64b15b36ff8f8e919
robertcal <robertcal900@gmail.com>
2019-12-19 12:42:11 +0900

    add ABC007_AB

---
+++ AtCoder/ABC/007/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+int main() {
+
+    int n; cin >> n;
+
+    cout << n - 1 << endl;
+}
