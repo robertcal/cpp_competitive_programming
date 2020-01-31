9d04fe9f3f286c1971869205e5091d5df1578103
robertcal <robertcal900@gmail.com>
2020-01-31 12:53:42 +0900

    add ABC127_D

---
+++ AtCoder/ABC/127/D.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+int main() {
+
+    int n, m; cin >> n >> m;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    
+}
