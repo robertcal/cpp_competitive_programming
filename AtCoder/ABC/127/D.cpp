26891fe4c9a0230b22fad5f6c96a6c442e553fac
robertcal <robertcal900@gmail.com>
2020-02-01 22:56:37 +0900

    update ABC127_D

--- AtCoder/ABC/127/D.cpp
+++ AtCoder/ABC/127/D.cpp
@@ -10,10 +10,30 @@ int main() {
 
     int n, m; cin >> n >> m;
 
-    vector<int> a(n);
+    map<int, int, greater<int>> ma;
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+        int a; cin >> a;
+        ma[a]++;
     }
 
-    
+    for (int i = 0; i < m; ++i) {
+        int b, c; cin >> b >> c;
+
+        ma[c] += b;
+    }
+
+    ll ans = 0;
+    int cnt = 0;
+
+    for (auto t : ma) {
+        if (cnt + t.second > n) {
+            ans += (ll)(n - cnt) * (ll)t.first;
+            break;
+        } else {
+            ans += (ll)t.first * (ll)t.second;
+            cnt += t.second;
+        }
+    }
+
+    cout << ans << endl;
 }
