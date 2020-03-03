7127d7c546e248afcece247769e8574928eab4e7
robertcal <robertcal900@gmail.com>
2020-03-03 19:02:15 +0900

    update ABC157_D

---
+++ AtCoder/ABC/157/D_after1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+struct UnionFind {
+    vector<int> par;
+
+    //正の場合、親を表す
+    //負の場合、根で数値は連結成分のサイズ
+    UnionFind(int n) : par(n, -1) {}
+
+
+};
+
+int main() {
+
+
+}
