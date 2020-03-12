cd8b9e34e6a55e83575bafa83ea65e9f4764c807
robertcal <robertcal900@gmail.com>
2020-03-12 23:23:48 +0900

    add AGC037_A(WA)

---
+++ AtCoder/AGC/037/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+int main() {
+
+    string s; cin >> s;
+
+    vector<string> v;
+
+    for (int i = 0; i < s.size(); ++i) {
+        string t = "";
+
+        for (int j = i; i + j < s.size(); ++j) {
+            t += s[j];
+
+            if (v.size() == 0) {
+                v.push_back(t);
+                continue;
+            }
+
+            if (v[v.size() - 1] != t) {
+                v.push_back(t);
+
+                i = j;
+            }
+        }
+    }
+
+    cout << v.size() << endl;
+}
