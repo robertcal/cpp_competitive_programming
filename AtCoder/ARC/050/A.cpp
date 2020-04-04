00e1eb337dfab8a260b8617d7e56813df0d4d569
robertcal <robertcal900@gmail.com>
2020-04-04 12:03:00 +0900

    add ARC050_A

---
+++ AtCoder/ARC/050/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+int main() {
+
+    char a, b; cin >> a >> b;
+
+    if (tolower(a) == b) {
+        puts("Yes");
+    } else {
+        puts("No");
+    }
+}
