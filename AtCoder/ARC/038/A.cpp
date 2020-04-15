ebf514315d2b981d0c760f957d4e1da8c7e0f1f3
robertcal <robertcal900@gmail.com>
2020-04-15 23:13:55 +0900

    add ARC038_A

---
+++ AtCoder/ARC/038/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+int main() {
+
+    int n; cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    sort(a.rbegin(), a.rend());
+
+    int ans = 0;
+    for (int i = 0; i < n ; i += 2) {
+        ans += a[i];
+    }
+
+    cout << ans << endl;
+}
