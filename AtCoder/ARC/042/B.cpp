d1633dbcd9afd8f48565579848c2882e00da9d67
robertcal <robertcal900@gmail.com>
2020-04-18 21:01:13 +0900

    add ARC042_B(途中)

---
+++ AtCoder/ARC/042/B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+int main() {
+
+    int a, b; cin >> a >> b;
+    int n; cin >> n;
+
+    vector<int> x(n);
+    vector<int> y(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> x[i] >> y[i];
+    }
+
+    double ans = (double)INF;
+
+    for (int i = 1; i < n; ++i) {
+        
+    }
+}
