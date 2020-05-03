70dd2e6fa57cd311ec90111d46bce0580cd7c1db
robertcal <robertcal900@gmail.com>
2020-05-03 17:57:33 +0900

    add ABC126_E

--- AtCoder/ABC/126/E.cpp
+++ AtCoder/ABC/126/E.cpp
@@ -1,9 +1,28 @@
 #include <bits/stdc++.h>
 
-#define INF 1e9
 using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
 
 int main() {
+
+    int n, m; cin >> n >> m;
+
+    vector<int> cnt(n, 0);
+    vector<vector<int>> v(n);
+
+    for (int i = 0; i < m; ++i) {
+        int x, y, z; cin >> x >> y >> z;
+        --x; --y;
+
+        v[x].push_back(y);
+        v[y].push_back(x);
+
+        cnt[x]++;
+        cnt[y]++;
+    }
 
 
 }
