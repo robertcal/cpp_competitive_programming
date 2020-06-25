45f9584b631c19035c00c802de03104f9ce5ab54
robertcal <robertcal900@gmail.com>
2020-06-25 22:18:28 +0900

    add ARC018_A

---
+++ AtCoder/ARC/018/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+int main() {
+
+    double h, b; cin >> h >> b;
+
+    double ans = b * h * h / 10000;
+
+    cout << fixed << setprecision(10) << ans << endl;
+}
