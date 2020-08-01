8e66a2634d44fdffe1fcd448c37b0c48a1a49eff
robertcal <robertcal900@gmail.com>
2020-08-01 13:58:55 +0900

    add ARC015_A

---
+++ AtCoder/ARC/015/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+
+int main() {
+
+    long double n; cin >> n;
+
+    long double ans = (9.0 / 5.0 * n) + 32.0;
+
+    cout << fixed << setprecision(10) << ans << endl;
+}
