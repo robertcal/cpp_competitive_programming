27d2aa0efba83d83395f9d5fed13250691b4b40f
robertcal <robertcal900@gmail.com>
2021-09-23 14:35:14 +0900

    add ABC037_C

---
+++ AtCoder/ABC/037/C_after2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n, k; cin >> n >> k;
+
+    vector<long long> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    // 閉区間で累積和
+    vector<long long> s(n);
+    s[0] = a[0];
+    for (int i = 0; i < n-1; ++i) {
+        s[i+1] = s[i] + a[i+1];
+    }
+
+    long long ans = 0;
+    ans += s[k-1];
+    for (int i = 0; i < n-k; ++i) {
+        ans += s[i+k] - s[i];
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
